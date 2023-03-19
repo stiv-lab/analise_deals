77473ad86fa5afc7efdf0ecb75e6ae0e7392862b
Ivan <starastin@gmail.com>
2023-03-19 23:01:48 +0300

    незаконченные изменения 190323

--- mod_deals.py
+++ mod_deals.py
@@ -11,13 +11,13 @@ class deal_transaction:
         self.account = row[1]
         self.name = row[4]
         self.kod = row[5]
-        amount = row[9]*row[13]
-        amount_point = row[11]*row[13]
+        amount = float(row[9])*int(row[13])
+        amount_point = float(row[11])*int(row[13])
         if row[7] == "Покупка": 
             self.bs = -1
             self.amount_dt = amount #row[16]
             self.amount_dt_point = amount_point
-            self.number_dt = row[13]
+            self.number_dt = int(row[13])
             self.amount_cr = 0
             self.amount_cr_point = 0
             self.number_cr = 0
@@ -25,17 +25,17 @@ class deal_transaction:
             self.bs = 1
             self.amount_cr = amount #row[16]
             self.amount_cr_point = amount_point
-            self.number_cr = row[13]
+            self.number_cr = int(row[13])
             self.amount_dt = 0
             self.amount_dt_point = 0
             self.number_dt = 0
-        self.price = row[9]
-        self.point_price = row[11]
-        self.numbers = row[13]*self.bs
+        self.price = float(row[9])
+        self.point_price = float(row[11])
+        self.numbers = int(row[13])*self.bs
         self.amount = amount*self.bs
         self.amount_point = amount_point*self.bs
         if row[17] == '' : self.commision = 0
-        else : self.commision = row[17]
+        else : self.commision = float(row[17])
         self.type = row[18]
         self.deal_close = row[0]
         #self.date_open_price = wdc.open_date_price(self.date)
